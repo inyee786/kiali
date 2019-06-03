8cb170524701bd80b9fb7621e73ecda06109acd9
inyee786 <intakhab.ali@mayadata.io>
2019-06-04 02:13:09 +0530

    update the path of github repo

--- build-script.sh
+++ build-script.sh
@@ -5,7 +5,7 @@ echo $GOPATH
 echo $GO_BUILD_ENVVARS
 # build kiali-ui
 cd ../
-git clone https://github.com/mayadata-io/kiali-ui.git
+git clone https://github.com/inyee786/kiali-ui.git
 cd kiali-ui/
 NODE_OPTIONS=--max_old_space_size=4096
 git checkout mayadata-io
