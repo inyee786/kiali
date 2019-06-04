38c0d8438181f873f8ff449088ae01d13383d936
inyee786 <intakhab.ali@mayadata.io>
2019-06-04 14:06:27 +0530

    update the travis and

--- build-script.sh
+++ build-script.sh
@@ -4,10 +4,14 @@ echo $KIALI_PATH
 echo $GOPATH
 echo $GO_BUILD_ENVVARS
 # build kiali-ui
+export NODE_OPTIONS=--max_old_space_size=4096
 cd ../
 git clone https://github.com/inyee786/kiali-ui.git
 cd kiali-ui/
 NODE_OPTIONS=--max_old_space_size=4096
 git checkout mayadata-io
-docker build -t mayadataio/kiali-ui:${COMMIT} .
+npm ci
+yarn build
+ls -ltr
+# docker build -t mayadataio/kiali-ui:${COMMIT} .
 # cd ../kiali
