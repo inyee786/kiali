d41a3c3bfdfe30d709c22773a8793c16d993a3ed
inyee786 <intakhab.ali@mayadata.io>
2019-06-04 14:20:59 +0530

    update the script

--- build-script.sh
+++ build-script.sh
@@ -10,7 +10,7 @@ git clone https://github.com/inyee786/kiali-ui.git
 cd kiali-ui/
 NODE_OPTIONS=--max_old_space_size=4096
 git checkout mayadata-io
-npm ci
+npm install
 yarn build
 ls -ltr
 # docker build -t mayadataio/kiali-ui:${COMMIT} .
